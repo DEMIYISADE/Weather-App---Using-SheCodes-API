7975ceb38ef4fd4c1e61c863318aa2294cd4026c
DEMIYISADE <bidemi.olayisade@gmail.com>
2023-04-26 04:36:39 +0100

    updating JavaScript with preliminary codes

---
+++ src/app.js
@@ -0,0 +1,13 @@
+
+let city = document.querySelector("#City");
+city.innerHTML = "New York" 
+console.log(city);
+let country = document.querySelector("#Country");
+country.innerHTML = "United States of America"
+console.log(country);
+let time = document.querySelector("#CurrentTime");
+time.innerHTML = "Wednesday, 04:04 AM";
+console.log(time);
+let icon = document.querySelector("#weatherIcon");
+icon.setAttribute ("src", "images/weather1.png");
+console.log(icon);
